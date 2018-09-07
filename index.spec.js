72b181ba8a2f8dc76ff7450dbffeee74b4c35f45
Xin Chen <xinszchen@thoughtworks.com>
2018-09-07 09:29:14 +0800

    test: more test cases to test specific content for 3-length string

--- index.spec.js
+++ index.spec.js
@@ -11,7 +11,7 @@ test('should return [a] when inputing a', () => {
 });
 
 test('should return [ab, ba] when inputing ab', () => {
-    expectedSet = new Set()
+    let expectedSet = new Set()
     expectedSet.add('ab')
     expectedSet.add('ba')
     expect(anagram('ab')).toEqual(expectedSet);
@@ -22,6 +22,16 @@ test('should return a lot when inputing abc', () => {
     expect([...anagram('abc')].length).toEqual(6);
 });
 
+test('should return specific set when inputing abc', () => {
+    let expectedSet = new Set()
+    expectedSet.add('abc')
+    expectedSet.add('acb')
+    expectedSet.add('bac')
+    expectedSet.add('bca')
+    expectedSet.add('cab')
+    expectedSet.add('cba')
+    expect(anagram('abc')).toEqual(expectedSet);
+});
 test('should return a lot when inputing abcd', () => {
     expect([...anagram('abcd')].length).toEqual(24);
 });
